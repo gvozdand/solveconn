41965993251242a6b643b845b724a4d77c7b4231
gvozdand <120746694+gvozdand@users.noreply.github.com>
2026-01-31 23:46:35 -0500

    Update parsing_engine.py

--- parsing_engine.py
+++ parsing_engine.py
@@ -1,34 +1,9 @@
-#This file is the parsing engine for gathering the needed values from the .txt file and organizing them accordingly for training/tes╭──────────────────────────────────────────────────╮
-    1                                                                                                                                     │  mason-lspconfig.nvim                  23:35:29 │
-    2                                                                                                                                     │━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━│
-    3 class parsing_engine:                                                                                                               │[mason-lspconfig.nvim] installing pyright         │
-    4 │   def __init__(self, text_file):                                                                                                  ╰──────────────────────────────────────────────────╯
-    5 │   │   self.text_file = text_file             ╭───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────╮
-    6 │                                              │  mason-lspconfig.nvim                                                                                                       23:35:29 │
-    7 │                                              │━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ │
-    8 │   def var_collect(self):                     │[mason-lspconfig.nvim] Server "tsserver" is not a valid entry in ensure_installed. Make sure to only provide lspconfig server names.   │
-    9 │   │                                          ╰───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────╯
-   10 │                                                                         ╭────────────────────────────────────────────────────────────────────────────────────────────────────────────╮
-   11 │   def varexo_collect(self):                                             │  mason-lspconfig.nvim                                                                            23:35:30 │
-   12 │                                                                         │━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ │
-   13 │   def parameter_collect(self):                                          │[mason-lspconfig.nvim] failed to install pyright. Installation logs are available in :Mason and :MasonLog   │
-   14 │                                                                         ╰────────────────────────────────────────────────────────────────────────────────────────────────────────────╯
-   15 │   def model_eq_collect(self):
-   16 │
-   17 │   def shock_collect(self):
-   18 │   │
-   19 │
-   20 │   def simul_collect(self):
-   21 │
-   22 │
-   23 │
-   24 │
-   25 │   with open(text_file, "r") as tf:
-   26 │   │   for line in tf:
-   27 │   │   │   #NOTE: each line here in the iterable (text file) is treated an independent string! Just keep this in mind moving forward...
-   28 │   │   │
-   29 │   │   │   for
-   30 │   │   │
-   31
-   32
-   33
+#This file is responsible for parsing the provided text file and organizing the system accordingly for prediction and simulations
+
+class parsing_engine:
+    def __init__(self):
+
+
+
+
+
